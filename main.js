737e6b8987e4c4dc1621f110b15a9ada9f1dcb31
Qirat-khalil <167483177+Qirat-khalil@users.noreply.github.com>
2024-04-21 16:22:58 -0700

    Add files via upload
    "A Simple Calculator in TypeScript" is a basic program that performs arithmetic operations such as addition, subtraction, multiplication, and division on two numbers input by the user.

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let user = await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type: "number",
+        message: "Enter your first number"
+    },
+    {
+        name: "secondNumber",
+        type: "number",
+        message: "Enter your second number"
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "select one of the operators",
+        choices: ["addition", "subtraction", "multiplication", "division"]
+    },
+]);
+if (user.operator === "addition") {
+    console.log(user.firstNumber + user.secondNumber);
+}
+else if (user.operator === "subtraction") {
+    console.log(user.firstNumber - user.secondNumber);
+}
+else if (user.operator === "multiplication") {
+    console.log(user.firstNumber * user.secondNumber);
+}
+else if (user.operator === "division") {
+    console.log(user.firstNumber / user.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
